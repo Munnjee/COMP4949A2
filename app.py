c0bd1b522d4ae3be8cd004d653899dbe719d88da
Min Ji Seo <seominji92@gmail.com>
2025-04-04 07:35:00 -0700

    change default port

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def predict():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render uses PORT env variable
+    port = int(os.environ.get("PORT", 10000))  # Render uses PORT env variable
     app.run(debug=True, host="0.0.0.0", port=port)
